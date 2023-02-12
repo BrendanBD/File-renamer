82d0f87e732dd770d05c028a6b5623a43ae64021
dalpa <dalpa@DESKTOP-388H7UT>
2023-02-12 08:40:51 -0500

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a folder containing PNG or DDS files to rename:");
+
+            string folderPath = Console.ReadLine();
+
+            string[] fileNames = Directory.GetFiles(folderPath);
+
+            foreach (string fileName in fileNames)
+            {
+                string fileExtension = Path.GetExtension(fileName);
+                if (fileExtension == ".png" || fileExtension == ".dds")
+                {
+                    string newFileName = "File" + Path.GetFileName(fileName);
+                    string newFilePath = Path.Combine(Path.GetDirectoryName(fileName), newFileName);
+                    File.Move(fileName, newFilePath);
+                }
+            }
+
+            Console.WriteLine("Files renamed successfully!");
+            Console.ReadLine();
+        }
+    }
+}
+
